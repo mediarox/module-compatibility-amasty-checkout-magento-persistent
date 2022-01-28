f9d80f015af6001774d75667a7d45e3d3895975b
Steven Fritzsche <sfritzsche@mediarox.de>
2022-01-28 10:12:29 +0100

    [!] dependency > jquery

--- view/frontend/web/js/view/form/element/email-mixin.js
+++ view/frontend/web/js/view/form/element/email-mixin.js
@@ -2,7 +2,9 @@
  * Copyright 2022 (c) mediarox UG (haftungsbeschraenkt) (http://www.mediarox.de)
  * See LICENSE for license details.
  */
-define(function () {
+define([
+    'jquery'
+],function ($) {
     'use strict';
 
     var mixin = {
